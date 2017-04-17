174ff592f5ac1f588e2ada7bfd6f8197186b6522
Greg Kops <greg@thinktopography.com>
2017-04-17 08:59:02 -0400

    adding more reducer tests

--- src/tests/index.js
+++ src/tests/index.js
@@ -2,9 +2,24 @@ import { expect } from 'chai'
 import reducer from '../reducer'
 import * as actionTypes from '../action_types'
 
-const testReducer = (state, action) => {
-  return { bar: 'baz' }
+const INITIAL_VALUE = { foo: 'bar' }
+
+const testReducer = (state = INITIAL_VALUE, action) => {
+
+  if(action.type === 'change') {
+
+    return {
+      foo: action.value
+    }
+
+  } else {
+
+    return state
+
+  }
 }
+
+const combined = reducer({ 'one.two.three': testReducer })
 
 describe('reducer', () => {
 
@@ -13,12 +28,45 @@ describe('reducer', () => {
       const action = {
         type: actionTypes.ADD,
         cid: 'a1b2',
-        namespace: 'foo'
+        namespace: 'one.two.three'
       }
 
-      const state = reducer({ 'foo': testReducer })(undefined, action)
+      const state = combined(undefined, action)
 
-      expect(state).to.be.eql({ foo: { a1b2: { bar: 'baz' } } })
+      expect(state).to.be.eql({ one: { two: { three: { a1b2: { foo: 'bar' } } } } })
+
+  })
+
+  it('mutates a singleton component', () => {
+
+    let state = combined(undefined, { type: actionTypes.ADD, namespace: 'one.two.three' })
+
+    const action = {
+      type: 'one.two.three/change',
+      value: 'baz'
+    }
+
+    state = combined(state, action)
+
+    expect(state).to.be.eql({ one: { two: { three: { foo: 'baz' } } } })
+
+  })
+
+  it('mutates the right cid', () => {
+
+    let state = combined(undefined, { type: actionTypes.ADD, namespace: 'one.two.three', cid: 'a1b2' })
+
+    state = combined(state, { type: actionTypes.ADD, namespace: 'one.two.three', cid: 'bz45' })
+
+    const action = {
+      type: 'one.two.three/change',
+      cid: 'bz45',
+      value: 'baz'
+    }
+
+    state = combined(state, action)
+
+    expect(state).to.be.eql({ one: { two: { three: { a1b2: { foo: 'bar' }, bz45: { foo: 'baz' } } } } })
 
   })
 
